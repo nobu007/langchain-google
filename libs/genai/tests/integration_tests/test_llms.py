80007a92546703479448ebcbb876477e7e11c7fb
jinno <klvx01@gmail.com>
2024-07-24 20:37:05 +0900

    fix: update for lint about chat_models.py

--- libs/genai/tests/integration_tests/test_llms.py
+++ libs/genai/tests/integration_tests/test_llms.py
@@ -7,9 +7,13 @@ Note: This test must be run with the GOOGLE_API_KEY environment variable set to 
 from typing import Dict, Generator
 
 import pytest
+from google.generativeai.types import (  # type: ignore[import]
+    HarmBlockThreshold,
+    HarmCategory,
+)
 from langchain_core.outputs import LLMResult
 
-from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
+from langchain_google_genai import GoogleGenerativeAI
 
 model_names = ["gemini-pro"]
 
