af7b6acc2d92144e2d707f3df422d676cc6f0cab
jinno <klvx01@gmail.com>
2024-09-01 14:44:47 +0900

    genai: accept BaseModel both Pydantic v1 and v2

--- libs/genai/langchain_google_genai/_function_utils.py
+++ libs/genai/langchain_google_genai/_function_utils.py
@@ -101,6 +101,45 @@ def get_pydantic_schema_v1(model: Type[BaseModel]) -> Dict:
     return schema_json
 
 
+def is_pydantic_model(cls: Type) -> bool:
+    # check attr for both Pydantic v1 and v2.
+    return hasattr(cls, "__fields__") and isinstance(cls.__fields__, dict)
+
+
+def get_pydantic_schema(model: Type[BaseModel]) -> Dict:
+    try:
+        # Pydantic v2
+        return get_pydantic_schema_v2(model)
+    except TypeError as e:
+        logger.debug("TypeError get_pydantic_schema_v2 e=%s", e)
+
+    try:
+        # Pydantic v1
+        return get_pydantic_schema_v1(model)
+    except TypeError as e:
+        logger.debug("TypeError get_pydantic_schema_v1 e=%s", e)
+
+    return {}
+
+
+def get_pydantic_schema_v2(model: Type[BaseModel]) -> Dict:
+    if hasattr(model, "model_json_schema"):
+        json_schema = model.model_json_schema()
+        return json_schema
+
+    raise TypeError(f"{model} is not Pydantic v2 model")
+
+
+def get_pydantic_schema_v1(model: Type[BaseModel]) -> Dict:
+    if hasattr(model, "schema_json"):
+        schema_dict = model.schema()
+        schema_json_str = json.dumps(schema_dict)
+    else:
+        raise TypeError(f"{model} is not Pydantic v1 model")
+    schema_json = json.loads(schema_json_str)
+    return schema_json
+
+
 class _ToolDictLike(TypedDict):
     function_declarations: _FunctionDeclarationLikeList
 
